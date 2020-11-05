14285b0686ed9f8d5852b87383905e9c14f2ffa9
unknown <georgeksharratt@gmail.com>
2020-11-05 13:56:52 +1300

    Added account updates and liking(sorta)

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,12 +1,38 @@
 $(document).ready(function(){
+
+	$('i.like').click(function(e) {
 	
+		e.stopPropagation();
+		e.preventDefault();
+	
+		var like 		= $(this).hasClass('far');
+		var image_id 	= $(this).data('image');
+		var _this 		= $(this);
+	
+		$.getJSON(
+			//URL, 
+			$SCRIPT_ROOT + '/like',
+			{
+				like: like,
+				image_id: image_id
+			}, 
+			function(result) {
+				//code to change the like icon
+				_this.removeClass('like fa-heart')
+				_this.removeClass('like fs-heart')
+			}
+		);
+	});
+
 	var $grid = $('.grid').masonry({
 		gutter: 30
 	});
+
 	// layout Masonry after each image loads
 	$grid.imagesLoaded().progress( function() {
 		$grid.masonry('layout');
 	});
+
 	$('.grid-item').click(function(){
 		var image_data = $(this).data(image);
 		var image = image_data.image;
